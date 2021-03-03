e15d72aee03086b372765f9ed3dfe0ae8bb955ba
Jae Hward <howardjake9512@gmail.com>
2021-03-03 12:09:25 -0500

    version 1.0: working base functionality and layout.

--- README.md
+++ README.md
@@ -14,6 +14,24 @@ A web-based app that displays the NYT Bestsellers lists.
 
 ![wireframev1](./img/wireframev1.png)
 
+## Pictures
+
+### Pre-Styling
+
+![Pre-Style](./img/Pre-Style.png)
+
+### Post-Styling (Large Screen)
+
+![Post-Style](./img/Post-Style.png)
+
+### Mid-size Screen
+
+![Mid-Size](./img/mid-size.png)
+
+### Mobile Screen
+
+![Mobile](./img/mobile.png)
+
 ## Future 
 
 - Enable a date function so users can go back and see past lists
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+// declares and intializes needed variables
 let BASE_URL, namesData, recoData, idx, currentList;
 let listIndex = []
-const API_KEY = `api-key=#`
+const API_KEY = `api-key=o9yKiaBtmcAU6APdCXJGOIKI42GxGHGy`
 
+// Requests a list of availble best sellers lists in the NYT API
 $.ajax({
     url: `https://api.nytimes.com/svc/books/v3/lists/names.json?${API_KEY}`
 }).then(
@@ -11,20 +13,15 @@ $.ajax({
         namesData.forEach(function(element, index) {
             listIndex.push(element.list_name_encoded);
             $(`select`).append(`<option id=${'name'+index}>${element.display_name}</option>`);
-            
-            
-        });  
+        });
+        // Sets the page to default the "Combined Print & E-Book Fiction"  
         idx = 0
         BASE_URL = `https://api.nytimes.com/svc/books/v3/lists/current/`+listIndex[idx]+`.json?` 
         handler();  
     }
 )
-    
 
-
-
-console.log(idx)
-
+// Dropdown menu changes will alter the BASE_URL to a different list
 $('#list-name').change(function () {
     $('select option:selected').each(function () {
         idx = $(this).index()
@@ -33,11 +30,9 @@ $('#list-name').change(function () {
         console.log(idx)
         handler();
     });    
-})
+});
 
-.change()
-
-
+// makes a request and prints the list data to the page 
 function handler (evt) {
     $.ajax({
         url: `${BASE_URL}${API_KEY}`
@@ -46,17 +41,12 @@ function handler (evt) {
             let currentList = data.results.books
             console.log(data);
             $('ol').html('');
-            currentList.forEach(function(element, index) {
-                $('ol').append(`<div id="list-item"><div id="img-box"><img src="${element.book_image}" id="image"></div>
-                <div id="info"><h4>${element.rank}) ${element.title}</h4><p>by ${element.author}</p><p id="description">${element.description}</p></div>`)
+            currentList.forEach(function(element) {
+                $('ol').append(`<div id="list-item"><div id="ranking">${element.rank}</div><div id="img-box"><div></div><a href="https://www.google.com/search?q=${element.title}+by+${element.author}" target="_blank"><img src="${element.book_image}" id="book_image"></a></div>
+                <div id="info"><a href="https://www.google.com/search?q=${element.title}+by+${element.author}" target="_blank"><p><strong> ${element.title}</strong></p></a><p>by ${element.author}</p><hr><p id="description">${element.description}</p><a target="_blank" href="${element.buy_links[0].url}">${element.buy_links[0].name}</a><br><a target="_blank" href="${element.buy_links[1].url}">${element.buy_links[1].name}</a><br><a target="_blank" href="${element.buy_links[2].url}">${element.buy_links[2].name}</a></div>`)
             });
         },
         (error) => {
             console.log(`failure to request!`);
         }
-    )
-} 
-
-function render() {
-    
-}
+    )} 
